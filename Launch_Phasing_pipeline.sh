b928bde3d95814c535142472d5a7b85f261b2d55
Simone Maestri <simone.maestri@hotmail.it>
2019-08-26 12:40:18 +0200

    Delete Launch_Phasing_pipeline.sh

--- Launch_Phasing_pipeline.sh
+++
@@ -1,6 +0,0 @@
-HOME_DIR=$1
-HOME_DIR_FULL=$(realpath $HOME_DIR)
-source activate ONToHap_env
-PIPELINE_DIR=$(realpath $( dirname "${BASH_SOURCE[0]}" ))
-nohup Rscript $PIPELINE_DIR/phasing_pipeline.R $HOME_DIR_FULL &
-
